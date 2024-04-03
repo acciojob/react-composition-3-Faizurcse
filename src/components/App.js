a91ec6db4968adddd7941481b6d87c3ccc0b897b
Faizurcse <mdfaizurrahman2000@gmail.com>
2024-04-04 03:42:44 +0530

    888

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,22 @@
 
-// import React from "react";
-// import './../styles/App.css';
 
-// const App = () => {
-//   return (
-//     <div>
-//         {/* Do not remove the main div */}
-//     </div>
-//   )
-// }
-
-// export default App
 import React from 'react';
 import Tooltip from './Tooltip';
-import './../styles/App.css';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Tooltip Example</h1>
       <p>
-        Hover over the text to see the tooltip: <span><Tooltip text="Hello, I'm a tooltip">Hover here</Tooltip></span>
+        Hover over the text to see the tooltip: <Tooltip text="Hello, I'm a tooltip">Hover here</Tooltip>
       </p>
-      <span><Tooltip text="Another tooltip"><button>Hover me</button></Tooltip></span>
+      <Tooltip text="Another tooltip">
+        <button>Hover me</button>
+      </Tooltip>
     </div>
   );
 }
+
 
 export default App;
